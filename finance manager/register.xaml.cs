ccd86175115b1a84a6751c65741025de4ec745e1
Michael <Michael_Hurschler@sluz.ch>
2025-02-10 10:40:55 +0100

    register formular erstellung

---
+++ finance manager/register.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace finance_manager
+{
+    /// <summary>
+    /// Interaktionslogik für register.xaml
+    /// </summary>
+    public partial class register : Window
+    {
+        public register()
+        {
+            InitializeComponent();
+        }
+sd
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+        }
+    }
+}
