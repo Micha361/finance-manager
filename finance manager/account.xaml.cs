2932b99225395f7a496337c9410ee7af2b00c073
Michael <Michael_Hurschler@sluz.ch>
2025-02-14 08:35:34 +0100

    hinzufügung von ,,zurück'' button funktion für account seite

--- finance manager/account.xaml.cs
+++ finance manager/account.xaml.cs
@@ -26,7 +26,10 @@ namespace finance_manager
 
     private void backbtn_Click(object sender, RoutedEventArgs e)
     {
+      Window1 mainApp = new Window1();
+      mainApp.Show();
 
-        }
+      this.Close();
+    }
     }
 }
