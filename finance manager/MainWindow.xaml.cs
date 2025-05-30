ce377f4eec8234bbb0bc0e8b0cfdd2cc1c7bb33c
Michael <Michael_Hurschler@sluz.ch>
2025-05-30 13:21:01 +0200

    hinzufügung von login mit enter funktion

--- finance manager/MainWindow.xaml.cs
+++ finance manager/MainWindow.xaml.cs
@@ -45,5 +45,14 @@ namespace finance_manager
       registerApp.Show();
       this.Close();
     }
+
+    private void passwordtxt_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+      if (e.Key == System.Windows.Input.Key.Enter)
+      {
+        loginbtn_Click(sender, e);
+      }
+    }
+
   }
 }
