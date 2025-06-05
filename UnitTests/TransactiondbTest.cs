70a1adbe2ee7ea884b268d7c5b72b2d0ffe32f36
Arber Idrizaj <Arber_Idrizaj@sluz.ch>
2025-06-05 21:31:30 +0200

    Balance test korrigiert. transactiontest hinzugefügt

---
+++ UnitTests/TransactiondbTest.cs
@@ -0,0 +1,29 @@
+﻿using finance_manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+  public class TransactiondbTest
+  {
+    [Fact]
+    public void AddTransaction_ShouldNotThrow()
+    {
+      
+      var db = new TransactionDb();
+      int testUserId = 9999;
+      new Balancedb().CreateBalanceForUser(testUserId, 1000);
+
+      
+      var exception = Record.Exception(() =>
+          db.AddTransaction(testUserId, 100, "income", "Testeintrag", DateTime.Now)
+      );
+
+      
+      Assert.Null(exception); 
+    }
+  }
+}
--- finance manager/transactiondb.cs
+++ finance manager/transactiondb.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 namespace finance_manager
 {
-    internal class TransactionDb
+    public class TransactionDb
     {
         private string dbPath;
         private string connectionString;
